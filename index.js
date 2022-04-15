8651ef84aa352404326e1cfe39680bdd25c20d25
willt <91817538+swillswindle@users.noreply.github.com>
2022-04-15 00:48:48 -0700

    forgot to save before last commit

--- index.js
+++ index.js
@@ -3,26 +3,6 @@ const Discord = require(`discord.js`); //import discord.js
 const fs = require("fs");
 const { REPL_MODE_STRICT } = require("repl");
 
-
-// const winston = require("winston");
-
-// const logConfiguration = {
-//   transports: [
-//     new winston.transports.File({
-//       level: 'error',
-//       filename: 'logs/error-logs.log'
-//   }),
-//   ],
-//   format: winston.format.combine(
-//     winston.format.timestamp({
-//       format: 'MMM-DD-YYYY HH:mm:ss'
-//     }),
-//     winston.format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`)
-//   )
-// };
-// module.exports.logger = winston.createLogger(logConfiguration);
-
-
 const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"],
 });
